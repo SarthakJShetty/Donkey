6423d35059cb6f2683b6b24b98e8a492c962c21f
root <root@WinBookAir.localdomain>
2017-08-09 02:20:23 +0530

    First Commit

--- donkey/config.py
+++ donkey/config.py
@@ -31,17 +31,18 @@ def parse_config(config_path):
     camera = config['camera']
     cfg['camera_loop_delay'] = camera.getfloat('loop_delay')
 
-    t_act = config['throttle_actuator']
-    cfg['throttle_actuator_channel'] = t_act.getint('channel')
-    cfg['throttle_actuator_min_pulse'] = t_act.getint('min_pulse')
-    cfg['throttle_actuator_max_pulse'] = t_act.getint('max_pulse')
-    cfg['throttle_actuator_zero_pulse'] = t_act.getint('zero_pulse')
+    t_act = config['left_actuator']
+    cfg['left_actuator_channel'] = t_act.getint('channel')
+    cfg['left_actuator_min_pulse'] = t_act.getint('min_pulse')
+    cfg['left_actuator_max_pulse'] = t_act.getint('max_pulse')
+    cfg['left_actuator_zero_pulse'] = t_act.getint('zero_pulse')
 
-    s_act = config['steering_actuator']
-    cfg['steering_actuator_channel'] = s_act.getint('channel')
-    cfg['steering_actuator_min_pulse'] = s_act.getint('left_pulse')
-    cfg['steering_actuator_max_pulse'] = s_act.getint('right_pulse')
-
+    s_act = config['right_actuator']
+    cfg['right_actuator_channel'] = t_act.getint('channel')
+    cfg['right_actuator_min_pulse'] = t_act.getint('min_pulse')
+    cfg['right_actuator_max_pulse'] = t_act.getint('max_pulse')
+    cfg['right_actuator_zero_pulse'] = t_act.getint('zero_pulse')
+    
     pilot = config['pilot']
     cfg['pilot_model_path'] = os.path.expanduser(pilot.get('model_path'))
 
--- scripts/diff_drive.py
+++ scripts/diff_drive.py
@@ -23,23 +23,30 @@ args = docopt(__doc__)
 if __name__ == '__main__':
 
     cfg = dk.config.parse_config('~/mydonkey/' + args['--config'] + '.ini')
-
+    remote_url = args['--remote']
+    if remote_url == None:
+        print('ERROR! : Must specify remote url')
+        sys.exit()
+    if not '//' in remote_url:
+        remote_url = 'http://'+remote_url
+    if not ':' in remote_url:
+        remote_url += ':8887' 
     remote_url = args['--remote']
 
-    left_motor = dk.actuators.Adafruit_Motor_Hat_Controller(cfg['left_actuator_channel'])
-    right_motor = dk.actuators.Adafruit_Motor_Hat_Controller(cfg['right_actuator_channel'])
+    left_motor = dk.actuators.Adafruit_Motor_Hat_Controller(0)
+    right_motor = dk.actuators.Adafruit_Motor_Hat_Controller(1)
     dd = dk.mixers.DifferentialDriveMixer(left_motor=left_motor,
                                  right_motor =right_motor)
 
     #asych img capture from picamera
     mycamera = dk.sensors.PiVideoStream()
-
+    myremote = dk.remotes.RemoteClient(remote_url, vehicle_id=cfg['vehicle_id'])
     #Get all autopilot signals from remote host
-    mypilot = dk.remotes.RemoteClient(remote_url, vehicle_id=vehicle_id=cfg['vehicle_id'])
-
+    mypilot = dk.remotes.RemoteClient(remote_url, vehicle_id=cfg['vehicle_id'])
+    mypilot.load()
     #Create your car
     car = dk.vehicles.BaseVehicle(drive_loop_delay=cfg['vehicle_loop_delay'],
-                                  camera=mycamera,
+                                  camera=mycamera,remote=myremote
                                   actuator_mixer=dd,
                                   pilot=mypilot)
     car.start()
